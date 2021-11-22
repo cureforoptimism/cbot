11ffa9a716d3b8fca2a7b5eba41828644f18e8e4
cure_for_optimism <cure_for_optimism@protonmail.com>
2021-11-21 22:30:30 -0700

    Create properties file for tokens

---
+++ src/main/java/com/cureforoptimism/cbot/services/TokenService.java
@@ -0,0 +1,31 @@
+package com.cureforoptimism.cbot.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Slf4j
+@PropertySource({"classpath:application.yml"})
+@Service
+public class TokenService {
+    @Value("${tokens.file}")
+    private String file;
+
+    public String getDiscordToken() {
+        try(InputStream input = new FileInputStream(file)) {
+            Properties properties = new Properties();
+            properties.load(input);
+            return properties.get("discord_access_token").toString();
+        } catch(IOException ex) {
+            log.error("Unable to retrieve Discord token", ex);
+        }
+
+        return null;
+    }
+}
