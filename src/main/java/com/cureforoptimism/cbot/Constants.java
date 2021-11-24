3ead4e17f2640c395e6b63d00139a3523bfdfd33
cureforoptimism <cure_for_optimism@protonmail.com>
2021-11-24 00:33:33 -0700

    Cleanup: make constant final

--- src/main/java/com/cureforoptimism/cbot/Constants.java
+++ src/main/java/com/cureforoptimism/cbot/Constants.java
@@ -4,6 +4,6 @@ import java.math.BigDecimal;
 
 public class Constants {
   public static final BigDecimal DEFAULT_STARTING_USD = new BigDecimal("20000.0");
-  public static long COIN_GECKO_CACHE_EXPIRY = 30 * 100; // 30 seconds
+  public static final long COIN_GECKO_CACHE_EXPIRY = 30 * 100; // 30 seconds
   public static final BigDecimal BUY_SELL_FEE_PCT = new BigDecimal("0.0035");
 }
